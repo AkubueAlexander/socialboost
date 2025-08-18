9656b4570fffabe4d6942c76af61e6cffe049480
AKUBUE ALEXANDER <info.dilistech@gmail.com>
2025-08-18 12:12:05 +0100

    earning-modify

--- earning-verify.php
+++ earning-verify.php
@@ -1,5 +1,26 @@
 <?php
+if (true) {
+    $amount = $_GET['amount'];
+    $userId = $_SESSION['id'];
 
+    $sqlCheck = 'SELECT * FROM earnerwallet  WHERE id = :id LIMIT 1';        
+    $stmtCheck = $pdo->prepare($sqlCheck);
+    $stmtCheck->execute(['id' => $id ]);
+    $rowCheck = $stmtCheck->fetch();
+
+    $balance = $rowCheck -> balance;
+
+    $balance -= $amount;
+
+    $stmt = $pdo->prepare("UPDATE earnerwallet SET balance = :balance WHERE userId = :userId");
+    $stmt->execute(['balance' => $balance, 'userId' => $userId]);
+
+
+    
+
+    
+
+}
 
 
 ?>
